57616310bb19cd680b19b3bdb63ee22d1371051a
nakanota1 <f1912201402@std.kitami-it.ac.jp>
2023-03-20 16:44:23 +0900

    add comment

--- oacc_template.fsx
+++ oacc_template.fsx
@@ -16,6 +16,21 @@ open Aqualis
 
  
 Compile [F] outputdir projectname fullversion <| fun () ->
+//-------------------------------------------------
+//データ転送する変数を宣言
+//ch.copyin_i <| fun (変数名) -> //CPU -> GPUに転送
+//ch.copyout_i <| fun (変数名) -> //GPU -> CPUに転送
+//通常の変数宣言に"copyin","copyout"がついたのみで後は同じ
+//-------------------------------------------------
+    ch.copyin_i1 1024 <| fun a ->
+        iter.num a.size1 <| fun i ->
+            a[i] <== i
+
+//-------------------------------------------------
+//並列化
+//oacc.parallelize <| fun () ->
+//    (並列領域)
+//-------------------------------------------------
     ch.i <| fun size ->
         size <== 1024
         ch.i1 size <| fun a ->
--- omp_template.fsx
+++ omp_template.fsx
@@ -14,31 +14,66 @@ let fullversion = preprocess.backup outputdir __SOURCE_DIRECTORY__ __SOURCE_FILE
  
 open Aqualis
 
+//配列の代入
 let sub(a:num1) = 
     iter.num a.size1 <| fun i ->
         a[i] <== i / 100
 
+//a~bのインデックスに変数を代入
 //let sub2(w:num1, a:int, b:int) =
 //    iter.range a b <| fun i ->
 //        w[i] <== i
 
+//sum,a,a+1,a+2,...,a+b-1,a+bの和を計算
 //let add(sum:num0, a:int, b:int) =
 //    iter.range a b <| fun i ->
 //        sum <== sum + i
  
 Compile [F] outputdir projectname fullversion <| fun () ->
+//-------------------------------------------------
+//プライベート変数の宣言
+//ch.private_i <| fun (変数名) ->
+//i: 整数, d: 小数, z: 複素数
+//通常の変数宣言に"private"がついたのみでそれ以外は同じ
+//-------------------------------------------------
+    ch.private_i <| fun num ->
+        num <== 0
+
+//-------------------------------------------------
+//並列化
+//omp.parallelize <| fun ->
+//    (並列領域)
+//-------------------------------------------------
     ch.d1 100 <| fun a ->
         omp.parallelize <| fun () ->
             sub (a)
 
+//-------------------------------------------------
+//指定のスレッド数で並列化
+//omp.parallelize (スレッド数) <| fun () ->
+//-------------------------------------------------
     //    omp.parallelize_th 4 <| fun () ->
     //        sub (a)
 
     //ch.i <| fun sum ->
     //    sum <== 0
+
+//-------------------------------------------------
+//和の計算を並列化
+//omp.parallelize (変数名) (演算子) <| fun () ->
+//-------------------------------------------------
     //    omp.reduction sum "+" <| fun () ->
     //        add (sum,1,100)
 
+//-------------------------------------------------
+//複数の処理を別々のスレッドで実行
+//omp.sections (スレッド数) <| fun () ->
+//    omp.section <| fun () ->
+//        (処理1)
+//    omp.section <| fun () ->
+//        (処理2)
+//    ...
+//-------------------------------------------------
     //    sum <== 0
     //    ch.i1 100 <| fun w ->
     //        omp.sections 4 <| fun () ->
